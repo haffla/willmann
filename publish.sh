cffb9118039a4c44d32b3b8bc933c22b19c7682d
Jakob Pupke <jakob.pupke@gmail.com>
2017-05-15 22:28:27 +0200

    Publishing to gh-pages (publish.sh)

--- publish.sh
+++ publish.sh
@@ -12,8 +12,11 @@ git worktree add -B gh-pages public upstream/gh-pages
 echo "Removing existing files"
 rm -rf public/*
 
+echo "Running brunch"
+brunch build --production
+
 echo "Generating site"
 hugo --baseURL "https://haffla.github.io/willmann"
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
